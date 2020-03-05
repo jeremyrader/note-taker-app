04f74ab44ebb75480149c1cff98c88c2396bcc6a
Jeremy Rader <jscottr8r@gmail.com>
2020-03-04 21:46:35 -0800

    setup parcel

--- components/simple-note.js
+++ components/simple-note.js
@@ -1,9 +1,11 @@
 import { LitElement, css, html } from 'lit-element'
+import * as feather from 'feather-icons'
 
 class SimpleNote extends LitElement {
 
   constructor() {
     super()
+    feather.replace()
   }
 
   static get properties() {
@@ -25,6 +27,7 @@ class SimpleNote extends LitElement {
           <div class="d-flex justify-content-end">
               <img src="images/edit.svg" alt="" width="32" height="32" title="Edit">
               <img src="images/x-circle.svg" alt="" width="32" height="32" title="Close">
+              <i data-feather="circle"></i>
           </div>
           ${
             this.edit
